9325cb68eb1f824b5fa4ab4e0e6be7eec486f9b9
Thinula Jayavihan <109309014+edmthinula@users.noreply.github.com>
2024-08-07 00:44:28 +0530

    mobile navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   ButtonGroup,
+  Spacer,
   Flex,
   Grid,
   GridItem,
@@ -9,6 +10,18 @@ import {
   Show,
   useStatStyles,
 } from "@chakra-ui/react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Input,
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenereList from "./components/GenereList";
@@ -18,6 +31,8 @@ import { PlatformSelector } from "./components/PlatformSelector";
 import { platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
+import Mobilenavbar from "./components/Mobilenavbar";
+import React from "react";
 
 export interface GameQuery {
   genere: Genere | null;
@@ -28,6 +43,9 @@ export interface GameQuery {
 
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
 
   return (
     <Grid
@@ -53,7 +71,40 @@ function App() {
       </Show>
       <GridItem area="main">
         <Box paddingLeft={2}>
+          <Flex>
           <GameHeading gameQuery={gameQuery}/>
+          <Spacer/>
+          <Show breakpoint='(max-width: 977px)'>
+
+          <Button ref={btnRef} colorScheme='teal' onClick={onOpen} marginRight={4}>
+          <HamburgerIcon/>
+        </Button>
+        <Drawer
+          isOpen={isOpen}
+          placement='right'
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader><GameHeading gameQuery={gameQuery}/></DrawerHeader>
+  
+            <DrawerBody>
+            <GenereList
+            selectedGenre={gameQuery.genere}
+            onSelectGenre={(genere: Genere) =>
+              setGameQuery({ ...gameQuery, genere })
+            }
+          />
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+
+
+
+          </Show>
+          </Flex>
           <Flex  marginBottom={5}>
             <Box marginRight={5}>
               <PlatformSelector
